43e2a616869023cb7a3206174f4d2811c3f4e32f
BG00000 <abakergabb@gmail.com>
2023-10-04 17:19:58 +1100

    add function

--- get_titles/script.js
+++ get_titles/script.js
@@ -1,13 +1,26 @@
 const books = [
-    {
-      title: 'Book1',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
+  {
+    title: 'Book1',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
 
-  
+function getTitles(arr) {
+
+  let newArr = []
+
+  for (i  = 0; i < arr.length; i++) {
+    newArr.push(arr[i].title)
+  }
+
+  return newArr
+
+}
+
+console.log(getTitles(books))
+
